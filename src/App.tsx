be3f6d82622d6ea8d5fa30078b1c355208fefd6a
ERASMO_DE_JESUS_CASAS <erasmo@vinneren.com.mx>
2022-10-04 19:07:31 -0500

    [INIT] start project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import Navigation from './routes/Navigation'
+
+function App() {
+
+  return (
+    <>
+    <Navigation />  
+    </>
+  )
+}
+
+export default App
